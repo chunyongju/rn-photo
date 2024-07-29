c5b978a2a21feac6de5b7ce29911bb5538a370cb
chunyongju <yjct90@gmail.com>
2024-07-29 18:30:36 +0900

    rn-photo 최초 커밋

---
+++ src/navigations/Navigation.js
@@ -0,0 +1,48 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
+import { initFirebase } from '../api/firebase';
+import { useUserState } from '../contexts/UserContext';
+import MainStack from './MainStack';
+
+const Navigation = () => {
+  const [user] = useUserState();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
+
+        const app = initFirebase();
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer onReady={onReady}>
+      {user.uid ? <MainStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
---
+++ src/navigations/routes.js
@@ -0,0 +1,8 @@
+export const AuthRoutes = {
+  SIGN_IN: 'SignIn',
+  SIGN_UP: 'SignUp',
+};
+
+export const MainRoutes = {
+  PROFILE: 'Profile',
+};
